d24fe9b67616af0f1fa9da707877585e350189be
Jacob Lam <jacob.lam@capco.com>
2018-10-30 16:07:04 -0400

    additions to holdings and composition

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,193 @@
-const searchTicker = (function() {
+const searchTicker = (function () {
   const tickerSearch = document.querySelector("#tickerSearch");
   const form = document.querySelector("#search");
   const resultsDiv = document.querySelector("#results");
-  const searchForm = document.querySelector("#search");
+  const bottomSearchBar = document.querySelector(".bottom-search-bar");
+  const holdingsTitle = document.querySelector("#holdings");
+  const compTitle = document.querySelector("#composition");
+  const hold = document.querySelector(".hold");
 
-  const addToHoldingsButton = function() {
-    searchForm.insertAdjacentHTML(
+  const addToHoldingsButton = function () {
+    form.insertAdjacentHTML(
       "afterend",
       `<button id="addToHoldings">Add to Holdings</button>`
     );
   };
 
+  const errorReturn = error => {
+    resultsDiv.innerHTML = "";
+    resultsDiv.insertAdjacentHTML(
+      "afterbegin",
+      `<div id="no-results">No results matched your search</div>`
+    );
+  };
+
+  const holdingsEvents = function () {
+    const tickerValue = tickerSearch.value.toUpperCase();
+    let recentPrice = 0;
+    // AJAX call to get the most recent price
+    fetch(
+      `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${tickerValue}&interval=5min&apikey=11457D37ACGLX94V`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        recentPrice = getLastPrice(data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    const getLastPrice = function (data) {
+      const objKeys = Object.keys(data["Time Series (5min)"]);
+      const firstKey = objKeys[0];
+      return (lastPrice = Number(
+        data["Time Series (5min)"][firstKey]["4. close"]
+      ).toFixed(2));
+    };
+
+    bottomSearchBar.addEventListener("click", function (e) {
+      const holdingsButton = e.target;
+
+      if (holdingsButton.matches("#addToHoldings")) {
+        if (holdingsTitle.nextSibling.innerHTML === undefined) {
+          holdingsTitle.insertAdjacentHTML(
+            "afterend",
+            `<div class="holdings">
+        <div class="tbl-subtotal-separator">
+          <table id="holdings-table">
+            <tr id="holdings-headings">
+              <th>STOCK</th>
+              <th>PRICE</th>
+              <th>QUANTITY</th>
+            </tr>
+            <tr>
+              <td>${tickerValue}</td>
+              <td>$${recentPrice}</td>
+              <td id="holdings-price">0
+              </td>
+              <td>
+                <i class="fas fa-arrow-up"></i>
+              </td>
+              <td>
+                <i class="fas fa-arrow-down"></i>
+              </td>
+              <td>
+                <i class="fas fa-times"></i>
+              </td>
+            </tr>
+          </table>
+        </div>
+      <div class="subtotals">
+        <div class="subtotal">
+          <h5>Subtotal</h5>
+          <p id="subtotal-amount"></p>
+        </div>
+        <div class="taxes">
+          <h5>Taxes & Fees</h5>
+          <p id="tf-amount"></p>
+        </div>
+        <div class="total">
+          <h5>Total</h5>
+          <p id="total-amount"></p>
+        </div>
+      </div>
+    </div>`
+          );
+          hold.addEventListener("click", function (e) {
+            const clicked = e.target;
+            if (clicked.matches(".fa-arrow-up")) {
+              let innerQuant = document.querySelector("#holdings-price").innerHTML;
+
+              // update     
+              let updatedQuant = Number(innerQuant) + 1;
+              quantity = updatedQuant;
+              document.querySelector("#holdings-price").innerHTML = updatedQuant;
+              // cannot do 'innerQuant = updatedQuant' b/c innerQuant only contains the value, not the element
+
+              // update subtotal
+              let updatedSubtotal = recentPrice * updatedQuant;
+              document.querySelector('#subtotal-amount').innerHTML = `${updatedSubtotal.toFixed(2)}`;
+
+              // update taxes & fees
+              let taxesFeesUpdated = updatedSubtotal * 0.13;
+              document.querySelector('#tf-amount').innerHTML = `${taxesFeesUpdated.toFixed(2)}`;
+
+              // update total
+              let totalUpdated = updatedSubtotal + taxesFeesUpdated;
+              document.querySelector('#total-amount').innerHTML = `${totalUpdated.toFixed(2)}`;
+            }
+          });
+        }
+        if (compTitle.nextSibling.innerHTML === undefined) {
+          compTitle.insertAdjacentHTML(
+            "afterend",
+            `<div class="comp-wrapper">
+					<table class="summary">
+						<tr>
+							<th id="stock-heading">
+								Stock
+							</th>
+							<th id="total-holdings">
+								Total Holdings
+							</th>
+							<th id="portfolio-heading">
+								Portfolio %
+							</th>
+						</tr>
+						<tr>
+							<td id="td1"></td>
+							<td id="td2" class="totals"></td>
+							<td id="td3"></td>
+						</tr>
+					</table>
+				</div>`
+          );
+          hold.addEventListener("click", function (e) {
+            const clicked = e.target;
+            if (clicked.matches(".fa-arrow-up")) {
+              const ticker = document.querySelector("#td1");
+              const totalHoldings = document.querySelector('#td2');
+              const portPercent = document.querySelector('#td3');
+              const allHoldingAmounts = Array.prototype.slice.call(document.querySelectorAll('.totals'));
+
+              console.log(allHoldingAmounts);
+              const total = allHoldingAmounts.reduce((total, current) => {
+                const inner = Number(current.innerHTML);
+                return total + inner;
+              }, 0);
+              console.log(total);
+
+              ticker.innerHTML = tickerValue;
+              totalHoldings.innerHTML = document.querySelector('#total-amount').innerHTML;
+              portPercent.innerHTML = `${document.querySelector()}`;
+
+
+
+            }
+          });
+        }
+      }
+    });
+  };
+
   return {
-    getStockData: function() {
-      form.addEventListener("submit", function(e) {
+    getStockData: function () {
+      form.addEventListener("submit", function (e) {
         e.preventDefault();
         const tickerValue = tickerSearch.value.toUpperCase();
-
-        const errorReturn = error => {
-          resultsDiv.innerHTML = "";
-          resultsDiv.insertAdjacentHTML(
-            "afterbegin",
-            `<div id="no-results">No results matched your search</div>`
-          );
-        };
 
         fetch(
           `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${tickerValue}&interval=5min&apikey=11457D37ACGLX94V`
         )
-          .then(function(response) {
+          .then(function (response) {
             return response.json();
           })
-          .then(function(data) {
-            // console.log(data);
-            return addResults(data);
+          .then(function (data) {
+            addResults(data);
           })
-          .catch(function(e) {
+          .catch(function (e) {
             console.log(e);
             return errorReturn(e);
           });
@@ -47,7 +199,7 @@ const searchTicker = (function() {
           const mostRecentPrices = recentResults[lastResultKey]; // obj of most recent price updates
           const mostRecentPricesArray = [];
 
-          (function() {
+          (function () {
             if (document.querySelector("#addToHoldings")) {
               document.querySelector("#addToHoldings").remove();
             }
@@ -88,7 +240,7 @@ const searchTicker = (function() {
             </table>
           `
           );
-
+          //updating values of the stock prices above graph
           const stockPriceDataCells = document.querySelectorAll(".stockPrices");
           for (i = 0; i < stockPriceDataCells.length; i++) {
             stockPriceDataCells[i].innerHTML = mostRecentPricesArray[i];
@@ -98,18 +250,18 @@ const searchTicker = (function() {
         fetch(
           `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${tickerValue}&outputsize=full&apikey=11457D37ACGLX94V`
         )
-          .then(function(response) {
+          .then(function (response) {
             return response.json();
           })
-          .then(function(data) {
+          .then(function (data) {
             console.log(data);
             return createGraph(data);
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log(error);
           });
 
-        const createGraph = function(data) {
+        const createGraph = function (data) {
           const dataObject = data["Time Series (Daily)"];
           const chartArray = Object.keys(dataObject).map(timestamp => {
             return dataObject[timestamp]["4. close"];
@@ -167,9 +319,21 @@ const searchTicker = (function() {
             }
           });
         };
+
+        holdingsEvents();
       });
     }
   };
 })();
 
+const holdings = function () {
+  // const hold = document.querySelector(".hold");
+  // hold.addEventListener()
+};
+
+const composition = function () { };
+
 searchTicker.getStockData();
+// console.log(searchTicker.lastPrice);
+// searchTicker.addToHoldings();
+holdings();
