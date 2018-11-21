15a29825953605013430b68bb30bc6f2b46a3e2d
Jacob Lam <jacob.lam@capco.com>
2018-11-20 20:59:05 -0500

    final version

--- react-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const element = <h1>Hello World</h1>;
-// this will be compiled down to React.createElement that is created from the React module
-console.log(element);
-ReactDOM.render(element, document.getElementById("root"));
